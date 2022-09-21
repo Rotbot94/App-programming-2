b398a4419ef22482fa6e3cf28d91c5168f19f4f1
Ali <ais@efiware.com>
2022-09-21 13:23:33 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
+    { title: 'Lister og Persistent Data', url: '/folder/List', icon: 'list' },
+    { title: 'Sensor/Geolocation', url: '/folder/Geolocation', icon: 'globe' },
+    { title: 'Orientation', url: '/folder/Orientation', icon: 'phone-landscape' },
     { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
     { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
--- src/app/folder/folder.module.ts
+++ src/app/folder/folder.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { FolderPageRoutingModule } from './folder-routing.module';
-
 import { FolderPage } from './folder.page';
+import {ListComponent} from '../list/list.component';
+import {GeolocationComponent} from '../geolocation/geolocation.component';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import {OrientationComponent} from '../orientation/orientation.component';
+import {ScreenOrientation} from '@awesome-cordova-plugins/screen-orientation/ngx';
 
 @NgModule({
   imports: [
@@ -15,6 +17,7 @@ import { FolderPage } from './folder.page';
     IonicModule,
     FolderPageRoutingModule
   ],
-  declarations: [FolderPage]
+  declarations: [FolderPage, ListComponent, GeolocationComponent, OrientationComponent],
+  providers: [Geolocation, ScreenOrientation]
 })
 export class FolderPageModule {}
