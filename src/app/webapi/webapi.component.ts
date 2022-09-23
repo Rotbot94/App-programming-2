b9fee5c795513bfe914a87d3b1b90125b112fe13
Ali <ais@efiware.com>
2022-09-23 11:04:32 +0200

    more changes

--- src/app/webapi/webapi.component.ts
+++ src/app/webapi/webapi.component.ts
@@ -3,7 +3,6 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-webapi',
   templateUrl: './webapi.component.html',
-  styleUrls: ['./webapi.component.scss'],
 })
 export class WebapiComponent {
 
